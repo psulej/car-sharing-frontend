a51021685178bf241e267a691243f90a02a94e5c
psulej <sulejpiotrek@gmail.com>
2025-08-30 02:38:30 +0200

    feat: Generate router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CarsListComponent} from "./components/car/cars-list/cars-list.component";
+import {AddCarComponent} from "./components/car/add-car/add-car.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'cars', pathMatch: 'full'},
+  {path: 'cars', component: CarsListComponent},
+  // { path: 'cars/:id', component: CarDetailsComponent },
+  {path: 'add-car', component: AddCarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddCarComponent } from './components/car/add-car/add-car.component';
+import { CarsListComponent } from './components/car/cars-list/cars-list.component';
+import { ClientsListComponent } from './components/client/clients-list/clients-list.component';
+import { AddClientComponent } from './components/client/add-client/add-client.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCarComponent,
+    CarsListComponent,
+    ClientsListComponent,
+    AddClientComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
