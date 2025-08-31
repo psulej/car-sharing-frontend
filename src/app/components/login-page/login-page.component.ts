69541fb6faf2ca58b46dc41439548af9b9084a89
psulej <sulejpiotrek@gmail.com>
2025-08-31 19:21:06 +0200

    feat: Add login page

---
+++ src/app/components/login-page/login-page.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { StorageService } from '../../services/storage.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent implements OnInit {
+  username = '';
+  password = '';
+  errorMessage = '';
+  isLoggedIn = false;
+  isLoginFailed = false;
+  roles: string[] = [];
+
+  constructor(
+    private authService: AuthService,
+    private storageService: StorageService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    if (this.storageService.isLoggedIn()) {
+      this.isLoggedIn = true;
+      this.roles = this.storageService.getUser().roles;
+    }
+  }
+
+  onSubmit(): void {
+    this.authService.login(this.username, this.password).subscribe({
+      next: data => {
+        this.storageService.saveUser(data);
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.roles = this.storageService.getUser().roles;
+        this.router.navigate(['/cars']);
+      },
+      error: err => {
+        this.errorMessage = err.error?.message || 'Invalid username or password';
+        this.isLoginFailed = true;
+      }
+    });
+  }
+}
